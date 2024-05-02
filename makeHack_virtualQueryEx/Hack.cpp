c7b5d04eeca2bda7dc7dc6224f4f565281ebadd6
odd-job-man <dhtmdwls987@gmail.com>
2024-05-03 02:56:10 +0900

    Update Hack.cpp

--- makeHack_virtualQueryEx/Hack.cpp
+++ makeHack_virtualQueryEx/Hack.cpp
@@ -27,51 +27,52 @@ int main()
 	MEMORY_BASIC_INFORMATION mbi;
 
 	// 탐색 시작
-	bool isSuccessToModify = false;
 	while (pTargetMemoryPos < (UINT_PTR)si.lpMaximumApplicationAddress)
 	{
 		SIZE_T size = VirtualQueryEx(hTargetProcess, (LPCVOID)pTargetMemoryPos, &mbi, sizeof(mbi));
 
 		// 다른 프로세스와 공유하는 메모리는 찾지 않는다.
-		if (size == sizeof(mbi) && mbi.Type == MEM_PRIVATE && mbi.State == MEM_COMMIT)
+		if (!(size == sizeof(mbi) && mbi.Type == MEM_PRIVATE && mbi.State == MEM_COMMIT))
 		{
-			CPUWORD* pTempBuffer = new CPUWORD[mbi.RegionSize / sizeof(CPUWORD)];
-			BOOL bResult = ReadProcessMemory(hTargetProcess, (LPCVOID)pTargetMemoryPos, (LPVOID)pTempBuffer, mbi.RegionSize,NULL);
-			if (bResult)
-			{
-				printf("Read Process Memory Success\n addr : %x\n", pTargetMemoryPos);
-				for (DWORD i = 0; i < mbi.RegionSize / sizeof(CPUWORD); ++i)
-				{
-					if (pTempBuffer[i] == valueToFind) // 원하는 값을 찾음
-					{
-						printf("\nsuccess to find value!\nRegion size : %uKbytes", mbi.RegionSize / 1024);
-						UINT_PTR memAddrToModify = (UINT_PTR)mbi.BaseAddress + i * sizeof(CPUWORD);
-						printf("\nTarget Addr : %x", memAddrToModify);
-						SIZE_T NumberOfBytesWritten;
-						bResult = WriteProcessMemory(hTargetProcess, (LPVOID)memAddrToModify, &valueToModify, sizeof(valueToModify), &NumberOfBytesWritten);
-						if (!bResult) __debugbreak();
-						else if (NumberOfBytesWritten != sizeof(CPUWORD)) __debugbreak();
+			pTargetMemoryPos += (UINT_PTR)mbi.RegionSize;
+			continue;
+		}
 
-						printf("\nIf successful, press e to end the process : ");
-						char TerminateChar;
-						scanf_s(" %c", &TerminateChar, sizeof(char));
-						if (TerminateChar == 'e')
-						{
-							isSuccessToModify = true;
-							break; //for문 탈출
-						}
-					}
-				}
-				if (isSuccessToModify)
-					break; // while문 탈출
-			}
-			else
-			{
-				printf("\nRead Process Memory Failed\n addr : %x, protect attributes : %x, Err Code : %u \n", pTargetMemoryPos, mbi.Protect,GetLastError());
-			}
+		//적절한 메모리 구역을 찾앗기 때문에 해당 메모리를 버퍼에 읽어옴.
+		CPUWORD* pTempBuffer = new CPUWORD[mbi.RegionSize / sizeof(CPUWORD)];
+		BOOL bResult = ReadProcessMemory(hTargetProcess, (LPCVOID)pTargetMemoryPos, (LPVOID)pTempBuffer, mbi.RegionSize, NULL);
+		if (!bResult)
+		{
+			printf("\nRead Process Memory Failed\n addr : %x, protect attributes : %x, Err Code : %u \n", pTargetMemoryPos, mbi.Protect, GetLastError());
+			pTargetMemoryPos += (UINT_PTR)mbi.RegionSize;
 			delete[] pTempBuffer;
+			continue;
+		}
+		printf("Read Process Memory Success\n addr : %x\n", pTargetMemoryPos);
+		for (DWORD i = 0; i < mbi.RegionSize / sizeof(CPUWORD); ++i)
+		{
+			// 조작하고자 하는 메모리의 값과 일치하는 값을 발견함
+			if (pTempBuffer[i] == valueToFind) 
+			{
+				printf("\nsuccess to find value!\nRegion size : %uKbytes", mbi.RegionSize / 1024);
+				UINT_PTR memAddrToModify = (UINT_PTR)mbi.BaseAddress + i * sizeof(CPUWORD);
+				printf("\nTarget Addr : %x", memAddrToModify);
+				SIZE_T NumberOfBytesWritten;
+				bResult = WriteProcessMemory(hTargetProcess, (LPVOID)memAddrToModify, &valueToModify, sizeof(valueToModify), &NumberOfBytesWritten);
+				if (!bResult) __debugbreak();
+				else if (NumberOfBytesWritten != sizeof(CPUWORD)) __debugbreak();
+
+				printf("\nIf successful, press e to end the process : ");
+				char TerminateChar;
+				scanf_s(" %c", &TerminateChar, sizeof(char));
+				if (TerminateChar == 'e')
+				{
+					delete[] pTempBuffer;
+					return 0;
+				}
+			}
 		}
 		pTargetMemoryPos += (UINT_PTR)mbi.RegionSize;
 	}
-
+	return 0;
 }
